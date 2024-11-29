2f31cf3dd4f5fccee27b886ec7d4d61ee0745b92
Mdhuna r <rmidhuna4@gmail.com>
2024-11-29 09:22:15 +0530

    vercel json  updated

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port:5000,
-    proxy: {
-      // '/api': {
-      // target:'https://www.bookd.store',
-      // changeOrigin:true
-      // }
-      // '/api': {
-      // target:'http://localhost:8000',
-      // changeOrigin:true
-      // }
-    },
-    watch: {
-      usePolling:true
-    }
   }
 })
